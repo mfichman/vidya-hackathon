74ac269030e0c5f35d6d5283aa714feda3870184
Andrew Fichman <andrew.fichman@gmail.com>
2018-01-27 22:53:37 -0500

    added drone movement

--- src/main/java/vidya/State.java
+++ src/main/java/vidya/State.java
@@ -16,39 +16,45 @@ public class State {
     }
 
     public void populate() {
-        double speed = 10;
-        double xMax = 1000;
-        double yMax = 1000;
+        double speed = 100;
+        double numDronesLeft = 1;
+        double numDronesRight = 0;
 
         // Spawn left side drones
-        for(int i=0;i<100;++i) {
-            double x1 = Math.random()*xMax/10;
-            double y1 = Math.random()*yMax;
-            double x2 = xMax;
-            double y2 = yMax/2;
+        for(int i=0;i<numDronesLeft;++i) {
+            double x1 = Math.random()*Movable.MAX_X/10;
+            double y1 = Math.random()*Movable.MAX_Y;
+            double x2 = Movable.MAX_X;
+            double y2 = Movable.MAX_Y/2;
             double theta = Math.atan((y2-y1)/(x2-x1));
             double xVel = speed*Math.cos(theta);
             double yVel = speed*Math.sin(theta);
-            drones.add(new Drone(new Movable(x1,x2,xVel,yVel)));
+            Drone d = new Drone(new Movable(x1,y1,xVel,yVel));
+            d.targetXPos = x2;
+            d.targetYPos = y2;
+            drones.add(d);
         }
 
         // Spawn right side drones
-        for(int i=0;i<100;++i) {
-            double x1 = Math.random()*xMax/10+xMax/9;
-            double y1 = Math.random()*yMax;
-            double x2 = 0;
-            double y2 = yMax/2;
+        for(int i=0;i<numDronesRight;++i) {
+            double x1 = Math.random()*Movable.MAX_X/10+Movable.MAX_X/9;
+            double y1 = Math.random()*Movable.MAX_Y;
+            double x2 = Movable.MIN_X;
+            double y2 = Movable.MAX_Y/2;
             double theta = Math.atan((y2-y1)/(x2-x1));
             double xVel = speed*Math.cos(theta);
             double yVel = speed*Math.sin(theta);
-            drones.add(new Drone(new Movable(x1,x2,xVel,yVel)));
+            Drone d = new Drone(new Movable(x1,y1,xVel,yVel));
+            d.targetXPos = x2;
+            d.targetYPos = y2;
+            drones.add(d);
         }
     }
 
     public void step() {
         drones.forEach((d) -> {d.update(this);});
         droneLasers.forEach((l) -> {l.update(this);});
-        drones.removeIf((d) -> {return d.alive;});
-        droneLasers.removeIf((l) -> {return l.alive;});
+        drones.removeIf((d) -> {return !d.alive;});
+        droneLasers.removeIf((l) -> {return !l.alive;});
     }
 }
